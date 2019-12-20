a3a48b6d104e4de7a6a863947ebfe98630ab71bc
Aaron <aarongillies88@gmail.com>
2019-12-19 20:30:52 -0500

    added NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+            <div id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Skills</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                </ul>
+            </div>
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar;
