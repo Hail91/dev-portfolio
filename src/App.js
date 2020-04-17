2429aa83a3b258b06050c4ca7004d6f68984d9c7
Aaron <aarongillies88@gmail.com>
2020-04-17 18:10:22 -0400

    Set up basic header styling/animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css'
-import NavBar from './components/NavBar';
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
-
   return (
     <div>
       <NavBar />
-      <h1>I make the web stuff!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+
+const Header = () => {
+  const headerFadeIn = useSpring({
+    config: { friction: 15, tension: 60 },
+    marginTop: 100,
+    opacity: 1,
+    from: { opacity: 0, marginTop: -100 },
+  });
+  return (
+    <section className="welcome-section">
+      <animated.div style={headerFadeIn}>
+        <h1>Hey there! My name is Aaron...</h1>
+        <h1>Let's talk!</h1>
+      </animated.div>
+    </section>
+  );
+};
+
+export default Header;
