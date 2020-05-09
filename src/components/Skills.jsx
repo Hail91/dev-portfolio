2b19953bd7eaf7410122cc7171dacbdf86a73d89
Aaron <aarongillies88@gmail.com>
2020-05-09 16:29:08 -0400

    base styling of icon carousal finished

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,72 +8,130 @@ import { ReactComponent as HTML } from "./Icons/html.svg";
 import { ReactComponent as Css } from "./Icons/css3.svg";
 import { ReactComponent as Js } from "./Icons/js.svg";
 import { ReactComponent as Node } from "./Icons/node-js.svg";
-import { ReactComponent as Python } from "./Icons/python.svg";
 import { ReactComponent as ReactJS } from "./Icons/react.svg";
 import { ReactComponent as Sass } from "./Icons/sass.svg";
 // -------------------
 const Skills = () => {
-  const [Config, setConfig] = useState({ config: config.gentle });
+  const [Config] = useState({ config: config.gentle });
   const [slide, setSlide] = useState(0);
-  const [offsetRadius, setOffsetRadius] = useState(2);
-  const [navigation, setNavigation] = useState(true);
-  // state = {
-  //   goToSlide: 0,
-  //   offsetRadius: 2,
-  //   showNavigation: true,
-  //   config: config.gentle,
-  // };
+  const [offsetRadius] = useState(2);
+  const [navigation] = useState(false);
 
   const SkillsSlides = [
     {
       key: uuidv4(),
       content: (
-        <div>
-          <HTML />
-          <p>Python</p>
+        <div className="icon-container">
+          <HTML
+            style={{
+              height: "20rem",
+              width: "20rem",
+              color: "#E44D26",
+              cursor: "pointer",
+            }}
+          />
+          <p>HTML</p>
         </div>
       ),
     },
     {
       key: uuidv4(),
-      content: <img src="https://picsum.photos/800/802/?random" alt="2" />,
+      content: (
+        <div className="icon-container">
+          <Css
+            style={{
+              height: "20rem",
+              width: "20rem",
+              color: "2965f1",
+              cursor: "pointer",
+            }}
+          />
+          <p>CSS3</p>
+        </div>
+      ),
     },
     {
       key: uuidv4(),
-      content: <img src="https://picsum.photos/600/803/?random" alt="3" />,
+      content: (
+        <div className="icon-container">
+          <Sass
+            style={{
+              height: "20rem",
+              width: "20rem",
+              color: " #cd6799",
+              cursor: "pointer",
+            }}
+          />
+          <p>Sass</p>
+        </div>
+      ),
     },
     {
       key: uuidv4(),
-      content: <img src="https://picsum.photos/800/500/?random" alt="4" />,
+      content: (
+        <div className="icon-container">
+          <Js
+            style={{
+              height: "20rem",
+              width: "20rem",
+              color: "#f0db4f",
+              cursor: "pointer",
+            }}
+          />
+          <p>JavaScript</p>
+        </div>
+      ),
     },
     {
       key: uuidv4(),
-      content: <img src="https://picsum.photos/800/804/?random" alt="5" />,
+      content: (
+        <div className="icon-container">
+          <Node
+            style={{
+              height: "20rem",
+              width: "20rem",
+              color: "#3C873A",
+              cursor: "pointer",
+            }}
+          />
+          <p>NodeJS</p>
+        </div>
+      ),
     },
     {
       key: uuidv4(),
-      content: <img src="https://picsum.photos/500/800/?random" alt="6" />,
+      content: (
+        <div className="icon-container">
+          <ReactJS
+            style={{
+              height: "20rem",
+              width: "20rem",
+              color: "#61DBFB",
+              cursor: "pointer",
+            }}
+          />
+          <p>ReactJS</p>
+        </div>
+      ),
     },
     {
       key: uuidv4(),
-      content: <img src="https://picsum.photos/800/600/?random" alt="7" />,
-    },
-    {
-      key: uuidv4(),
-      content: <img src="https://picsum.photos/805/800/?random" alt="8" />,
+      content: (
+        <div className="icon-container">
+          <img
+            src="https://img.icons8.com/color/144/000000/python.png"
+            alt="logo"
+          />
+          <p>Python</p>
+        </div>
+      ),
     },
   ].map((slide, index) => {
-    return { ...slide, onClick: () => setSlide({ slide: index }) };
+    return { ...slide, onClick: () => setSlide(index) };
   });
 
-  // onChangeInput = (e) => {
-  //   this.setState({
-  //     [e.target.name]: parseInt(e.target.value, 10) || 0,
-  //   });
-  // };
-
   return (
-    <div style={{ width: "80%", height: "500px", margin: "0 auto" }}>
+    <div style={{ width: "50%", height: "500px", margin: "0 auto" }}>
       <Carousel
         slides={SkillsSlides}
         goToSlide={slide}
@@ -81,15 +139,6 @@ const Skills = () => {
         showNavigation={navigation}
         animationConfig={Config.config}
       />
-      <div
-        style={{
-          margin: "0 auto",
-          marginTop: "2rem",
-          width: "50%",
-          display: "flex",
-          justifyContent: "space-around",
-        }}
-      ></div>
     </div>
   );
 };
