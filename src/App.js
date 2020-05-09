157a4791e054437c4775e9770cd5337eedb3cecf
Aaron <aarongillies88@gmail.com>
2020-05-09 15:04:21 -0400

    begun to setup carousal

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar.jsx";
 import Header from "./components/Header.jsx";
+import Skills from "./components/Skills.jsx";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Header />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Carousel from "react-spring-3d-carousel";
+import { config } from "react-spring";
+
+const Skills = () => {
+  const [configuration, setConfiguration] = useState({
+    config: config.gentle,
+  });
+
+  const skillsSlide = [];
+  return (
+    <div>
+      <h1>Carousal will go here</h1>
+    </div>
+  );
+};
+
+export default Skills;
