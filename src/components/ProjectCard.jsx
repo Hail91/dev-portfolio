28afe1133751e275df0c9bc70cd479c25cd30ec2
Aaron <aarongillies88@gmail.com>
2020-05-09 18:36:23 -0400

    wip: finish building project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import ReactCardFlip from "react-card-flip";
 
+// Images
+
 const ProjectCard = () => {
   const [isFlipped, setIsFlipped] = useState(false);
 
@@ -12,12 +14,9 @@ const ProjectCard = () => {
     <div className="main-card">
       <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
         <div className="card-front">
-          <h1>Front</h1>
-          <img src="" alt="kansha-img" />
           <button onClick={handleFlip}>Click to flip</button>
         </div>
         <div className="card-back">
-          <h1>Back</h1>
           <button onClick={handleFlip}>Click to flip</button>
         </div>
       </ReactCardFlip>
