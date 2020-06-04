da14a4a19d8d837dc014650758e0e44a24bdcc95
Aaron <aarongillies88@gmail.com>
2020-06-04 15:42:02 -0400

    working on styling/formatting contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,37 +8,81 @@ const Contact = () => {
     email: "",
     message: "",
   });
+  // Function to handle input inside of form
+  const handleInput = (event) => {
+    event.preventDefault();
+    setContact({ ...contact, [event.target.name]: event.target.value });
+  };
+
+  // Function to send form data to backend API
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("https://portfolio-be-ag.herokuapp.com/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contact),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
+
   return (
     <div className="contact-container">
       <h1>Contact Form will go here</h1>
       <form className="contact-form">
         <div className="contact-form-name">
-          <label>First Name</label>
+          <div className="name-input">
+            <label>First Name</label>
+            <input
+              className="contact-input"
+              value={contact.firstName}
+              onChange={handleInput}
+              name="firstName"
+              placeholder="First Name"
+            ></input>
+          </div>
+          <div className="name-input">
+            <label>Last Name</label>
+            <input
+              className="contact-input"
+              value={contact.lastName}
+              onChange={handleInput}
+              name="lastName"
+              placeholder="Last Name"
+            ></input>
+          </div>
+        </div>
+        <div className="email-input">
+          <label>Email</label>
           <input
             className="contact-input"
-            name="firstName"
-            placeholder="First Name"
-          ></input>
-          <label>Last Name</label>
-          <input
-            className="contact-input"
-            name="lastName"
-            placeholder="Last Name"
+            value={contact.email}
+            onChange={handleInput}
+            name="email"
+            placeholder="Email"
           ></input>
         </div>
-        <label>Email</label>
-        <input
-          className="contact-input"
-          name="Email"
-          placeholder="Email"
-        ></input>
-        <label>Message</label>
-        <textarea
-          className="contact-input"
-          name="message"
-          placeholder="Message"
-        ></textarea>
+        <div className="message-input">
+          <label>Message</label>
+          <textarea
+            className="contact-input"
+            value={contact.message}
+            onChange={handleInput}
+            name="message"
+            placeholder="Message"
+          ></textarea>
+        </div>
       </form>
+      <button type="submit" onClick={handleSubmit}>
+        Send
+      </button>
     </div>
   );
 };
