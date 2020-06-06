7f4d5588211ba4d60407c6763d0a2095508535a7
Aaron <aarongillies88@gmail.com>
2020-06-06 14:52:20 -0400

    working on getting header buttons to fade in after text animation ends

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,17 +10,36 @@ const Header = () => {
     from: { opacity: 0, marginTop: -100 },
   });
 
+  // let info = [
+  //   "Hello, my name is Aaron! \nI'm a Full-Stack developer with a passion for \nproblem solving, and building engaging user experiences.",
+  //   "I'd love to get a conversation started!",
+  // ];
+
   return (
     <section className="welcome-section" id="welcome">
       <animated.div className="header-container" style={headerFadeIn}>
         <TypeIt
           element={"h2"}
-          options={{ cursor: false, breaklines: true, speed: 70 }}
+          options={{
+            cursor: false,
+            breaklines: true,
+            speed: 60,
+          }}
         >
-          Hello, my name is Aaron! {"\n"}I'm a Full-Stack developer with a
-          passion for {"\n"}problem solving, and building engaging user
-          experiences.
+          <p>
+            Hello, my name is Aaron! {"\n"}I'm a Full-Stack developer with a
+            passion for {"\n"}problem solving, and building engaging user
+            experiences.
+          </p>{" "}
+          {"\n"}
+          <p style={{ marginTop: "10px" }}>
+            I'd love to get a conversation started!
+          </p>
         </TypeIt>
+        <div className="header-btns-container">
+          <button style={{ marginRight: "20px" }}>View Resume (pdf)</button>
+          <button>View Work</button>
+        </div>
       </animated.div>
     </section>
   );
