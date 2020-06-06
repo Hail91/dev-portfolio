e32680683530cdbd14442181476ce40db158d6b1
Aaron <aarongillies88@gmail.com>
2020-06-06 12:00:54 -0400

    text dynamically typing in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSpring, animated } from "react-spring";
 
 const Header = () => {
@@ -8,10 +8,27 @@ const Header = () => {
     opacity: 1,
     from: { opacity: 0, marginTop: -100 },
   });
+
+  let count = 0;
+  let headerMsg = "Hey there! My name is Aaron...";
+  let typeSpeed = 40;
+
+  const write = () => {
+    if (count < headerMsg.length) {
+      document.getElementById("headerMsg").innerHTML += headerMsg.charAt(count);
+      count++;
+      setTimeout(write, typeSpeed);
+    }
+  };
+
+  useEffect(() => {
+    write();
+  }, []);
+
   return (
     <section className="welcome-section" id="welcome">
       <animated.div style={headerFadeIn}>
-        <h1>Hey there! My name is Aaron...</h1>
+        <p id="headerMsg"></p>
         <h1>Let's talk!</h1>
       </animated.div>
     </section>
